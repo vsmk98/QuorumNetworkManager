aa84910b5114e82f659c8fcde4384d15575a7e9f
Coenie Beyers <coeniebeyers@gmail.com>
2017-03-02 18:25:13 +0200

    Added script to start quorum voter only node

---
+++ startQuorumVoterNode.sh
@@ -0,0 +1,2 @@
+geth --identity $1 --rpc --rpcport "20000" --rpccorsdomain "localhost" --datadir Blockchain --port 20010 --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum" --rpcapi "db,eth,net,web3,admin,personal,quorum" --nat "any" --voteaccount $2 --votepassword $3 --minblocktime 1 --maxblocktime 2 --permissioned
+
