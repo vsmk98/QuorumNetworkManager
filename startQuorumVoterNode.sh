3379e00352e35bd4d83d4d3955bcaebcf34b671b
Coenie Beyers <coeniebeyers@gmail.com>
2017-03-05 20:59:58 +0200

    Creating PRIVATE_CONFIG variable

--- startQuorumVoterNode.sh
+++ startQuorumVoterNode.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+set -u
+set -e
+PRIVATE_CONFIG=constellation.config
 geth --identity $1 --rpc --rpcport "20000" --rpccorsdomain "localhost" --datadir Blockchain --port 20010 --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum" --rpcapi "db,eth,net,web3,admin,personal,quorum" --nat "any" --voteaccount $2 --votepassword $3 --minblocktime 1 --maxblocktime 2 --permissioned
 
