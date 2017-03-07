ecf6e8e2961b0f6bb5581769351118eec5d39783
Coenie Beyers <coeniebeyers@gmail.com>
2017-03-07 09:05:01 +0200

    shhh! let there be whisper.

---
+++ node2Whisper.js
@@ -0,0 +1,17 @@
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:20010'));
+
+web3.shh.filter({"topics":["TestTopic"]}).watch(function(err, msg) {
+  if(err){console.log("ERROR:", err);};
+  var message = hex2a(msg.payload);
+  console.log("Decoded message:", message);
+});
+
+function hex2a(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
