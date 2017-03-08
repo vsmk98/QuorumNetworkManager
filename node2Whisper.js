48b2687a5d6db49a0af97b0c6bc0719026b0abb3
Coenie Beyers <coeniebeyers@gmail.com>
2017-03-08 08:42:42 +0200

    Indentation fix

--- node2Whisper.js
+++ node2Whisper.js
@@ -1,17 +1,19 @@
 var Web3 = require('web3');
 var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:20010'));
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:40010'));
 
 web3.shh.filter({"topics":["TestTopic"]}).watch(function(err, msg) {
   if(err){console.log("ERROR:", err);};
   var message = hex2a(msg.payload);
-  console.log("Decoded message:", message);
+  console.log("New message:");
+  console.log(message);
 });
 
 function hex2a(hexx) {
-    var hex = hexx.toString();//force conversion
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
+  var hex = hexx.toString();//force conversion
+  var str = '';
+  for (var i = 0; i < hex.length; i += 2){
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
+  return str;
 }
