8c9ad5655c5a32b8f749a9265ceb15088962bc28
Coenie Beyers <coeniebeyers@gmail.com>
2017-03-08 10:04:11 +0200

    Added util.js

---
+++ util.js
@@ -0,0 +1,11 @@
+
+function hex2a(hexx) {
+  var hex = hexx.toString();//force conversion
+  var str = '';
+  for (var i = 0; i < hex.length; i += 2){
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
+  return str;
+}
+
+exports.Hex2a = hex2a;
