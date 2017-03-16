6cf81cc0bc46596f68c2f1a98ad37977ffad22f2
Coenie Beyers <coeniebeyers@gmail.com>
2017-03-16 16:19:08 +0200

    Can now push ~50 tx/s

--- doTransactions.js
+++ doTransactions.js
@@ -1,12 +1,13 @@
 var async = require('async');
 var util = require('./util.js');
-var count = 0;
+var confirmedCount = 0;
+var submittedCount = 0;
+var failedSubmissions = 0;
 var startTime = new Date().getTime();
 
 function startSubmittingTransactions(){
   var seqFunction = async.seq(
-    util.CreateWeb3Connection,
-    util.GetNewGethAccount
+    util.CreateWeb3Connection
   );
   var result = {
     "web3IPCHost": './Blockchain/geth.ipc',
@@ -17,14 +18,16 @@ function startSubmittingTransactions(){
     var web3IPC = res.web3IPC;
     var web3RPC = res.web3RPC;
 
-    var account1 = res.addressList[0];
+    //var account1 = res.addressList[0];
+
+    var txObj = {
+      from: web3RPC.eth.accounts[0],
+      to: web3RPC.eth.accounts[1],
+      value: 0
+    };
 
     setInterval(function(){
-      var txObj = {
-        from: web3RPC.eth.accounts[0],
-        to: account1,
-        value: 1
-      };
+      submittedCount++;
       web3IPC.eth.sendTransaction(txObj, function(err, txHash){
         if (err) { console.log('Send transaction ERROR:', err); }
         setTimeout(function(){
@@ -32,28 +35,28 @@ function startSubmittingTransactions(){
             if (err) { console.log('Get transaction ERROR:', err); }
             if(tx == undefined || tx.blockNumber == null){
               console.log('Tx not included in block:', txHash);
+              failedSubmissions++;
             } else {
-              count++;
+              confirmedCount++;
             }
           });          
         }, 3000);
       });
-    }, 1000);
+    }, 20);
   });
 }
 
 function run(){
-  for(var i = 0; i < 3; i++){
-    var timeout = Math.floor(Math.random()*10*1000);
-    setTimeout(function(){
-      startSubmittingTransactions()
-    }, timeout);
-  }
+
+  startSubmittingTransactions();
 
   setInterval(function(){
     var elapsedTime = new Date().getTime() - startTime;
-    console.log('Confirmed transactions:', count);
-    console.log('Confirmed tx/s', (count/(elapsedTime/1000)).toFixed(2)); 
+    console.log('Confirmed tx/s', (confirmedCount/(elapsedTime/1000)).toFixed(2)); 
+    console.log('Submitted tx/s', (submittedCount/(elapsedTime/1000)).toFixed(2)); 
+    console.log('Submitted: '+submittedCount+' | Confirmed: '+confirmedCount);
+    console.log('Failed submissions:', failedSubmissions);
+    console.log('-');
   }, 10*1000);
 }
 
