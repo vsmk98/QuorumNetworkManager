656ac1424d0145bc4186b102ee8e09727c26fbee
Coenie Beyers <coeniebeyers@gmail.com>
2017-04-04 07:21:33 +0200

    Added any rpc cors domain

--- startQuorumParticipantNode.sh
+++ startQuorumParticipantNode.sh
@@ -11,7 +11,7 @@ GLOBAL_ARGS="--networkid $NETID --shh --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,
 nohup constellation-node constellation.config &> constellation.log &
 sleep 5
 
-PRIVATE_CONFIG=constellation.config nohup geth --datadir Blockchain $GLOBAL_ARGS --rpcport 20010 --port 20000 &> gethNode.log &
+PRIVATE_CONFIG=constellation.config nohup geth --datadir Blockchain $GLOBAL_ARGS --rpcport 20010 --rpccorsdomain "*" --port 20000 &> gethNode.log &
 sleep 5
 
 echo "[*] Node started"
