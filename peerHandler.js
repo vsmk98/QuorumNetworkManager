9add3fdd49085c0750e457cfbe4be9c746169a0b
Coenie Beyers <coeniebeyers@gmail.com>
2017-06-05 22:55:52 +0200

    Removed console logging

--- peerHandler.js
+++ peerHandler.js
@@ -3,7 +3,6 @@ let events = require('./eventEmitter.js');
 function listenForNewEnodes(result, cb){
   let web3IPC = result.web3IPC
   events.on('newEnode', function(enode){
-    console.log('new enode event:', enode)
     web3IPC.admin.addPeer(enode, function(err, res){
       if(err){console.log('ERROR:', err)}
     })
