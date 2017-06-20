75ac2fce261e46388369979821cea30e252c1dff
Coenie Beyers <coeniebeyers@gmail.com>
2017-06-20 12:54:17 +0800

    Only addPeer for new enodes

--- peerHandler.js
+++ peerHandler.js
@@ -1,11 +1,16 @@
 let events = require('./eventEmitter.js');
 
+let enodeList = []
+
 function listenForNewEnodes(result, cb){
   let web3IPC = result.web3IPC
   events.on('newEnode', function(enode){
-    web3IPC.admin.addPeer(enode, function(err, res){
-      if(err){console.log('ERROR:', err)}
-    })
+    if(enodeList.indexOf(enode) < 0){
+      enodeList.push(enode)
+      web3IPC.admin.addPeer(enode, function(err, res){
+        if(err){console.log('ERROR:', err)}
+      })
+    }
   })
   cb(null, result)
 }
